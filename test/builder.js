0e764460de85cefa2aed8bae6f46cd1f9fea281f
ajlopez <webmaster@ajlopez.com>
2012-06-07 07:30:23 -0300

    Builder setSpout, getSpout, with tests

--- test/builder.js
+++ test/builder.js
@@ -13,3 +13,15 @@ exports['Create Topology'] = function(test) {
 	test.ok(topology);
 	test.done();
 }
+
+exports['Set and Get Spout'] = function(test) {
+	var builder = ss.createTopologyBuilder();
+    var spout = {};
+    builder.setSpout("myspout", spout);
+    
+    test.ok(builder.getSpout("myspout"));
+    test.equal(builder.getSpout("myspout"), spout);
+    
+	test.done();
+}
+
