ceff2700a87d3a61cde4113f304bfd3bc9db686b
ajlopez <webmaster@ajlopez.com>
2012-06-07 19:26:25 -0300

    shuffleGrouping name, with test

--- test/bolt.js
+++ test/bolt.js
@@ -12,3 +12,14 @@ exports['Set and Get Bolt'] = function(test) {
 	test.done();
 }
 
+exports['Set Shuffle Grouping'] = function(test) {
+	var builder = ss.createTopologyBuilder();
+    var bolt = {};
+    
+    var result = builder.setBolt("mybolt", bolt);
+    result.shuffleGrouping("myspout");
+    test.equal(result.getGroupingName(), "myspout");
+    
+	test.done();
+}
+
