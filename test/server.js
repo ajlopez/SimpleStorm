4fcaa007b4cf47bc3936969b73ac4f06c6ea134c
ajlopez <webmaster@ajlopez.com>
2013-01-26 18:53:01 -0300

    topology.listen with first test

--- test/server.js
+++ test/server.js
@@ -15,7 +15,8 @@ exports['Topology connect to Server'] = function(test)
 	
     var topology = builder.createTopology();
 	topology.start();
-    
+
+    // messages from server
     var server = simplemessages.createServer(function (client) {
         for (var k = 0; k < 3; k++)
             client.write({ target: "bolt", message: 1 });
@@ -24,6 +25,42 @@ exports['Topology connect to Server'] = function(test)
     server.listen(3000);
     
     topology.connectToServer(3000);
+    
+    function done() {
+        topology.stop();
+        server.close();
+    }
+}
+
+exports['Public Topology connect to Server'] = function(test)
+{
+    test.expect(9 + 3);
+	var builder = ss.createTopologyBuilder();
+    var result = { sum: 0, total: 3 };
+    
+    // No messages from local Spout
+    builder.setSpout("spout", new Spout(0, 1));
+    builder.setBolt("bolt", new Bolt(test, 1, result, done)).shuffleGrouping("spout");
+	
+    var topology = builder.createTopology();
+	topology.start();
+
+    // messages from server
+    var server = simplemessages.createServer(function (client) {
+        client.on('data', function (msg) {
+            test.ok(msg);
+            test.equal(msg.port, 3001);
+            test.equal(msg.host, 'localhost');
+            
+            for (var k = 0; k < 3; k++)
+                client.write({ target: "bolt", message: 1 });
+        });
+    });
+    
+    server.listen(3000);
+    
+    topology.connectToServer(3000);
+    topology.listen(3001);
     
     function done() {
         topology.stop();
